9e668f68dba44a3730b773f43b49d000db664773
Harry Gee <58229292+harrygman@users.noreply.github.com>
2022-04-26 15:06:47 +0100

    Four tests working and passing - income tax calculator complete.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,19 @@
 module Lib where
 
+defaultUsersTakings :: Int
+defaultUsersTakings = 12570
+
+userTakings :: Int -> Int
+userTakings income = defaultUsersTakings
+
 incomeTax :: Int -> Int 
 incomeTax income = 
-    if income < 12570
+    if income < userTakings income
         then 0
-        else (income - 12570) `div` 5
+        else if income < 50270
+            then  (income - userTakings income) `div` 5
+            else if income < 150000 
+                then ((50270 - userTakings income) `div` 5) + ((income - 50270) * 4) `div` 10
+                else ((50270 - userTakings income) `div` 5) 
+                + ((150000 - 50270) * 4) `div` 10 
+                + ((income - 150000) * 45) `div` 100
--- test/Spec.hs
+++ test/Spec.hs
@@ -3,6 +3,7 @@ import Test.HUnit -- Import testing framework
 import Lib -- Import file holding the functions
 
 
+
 main :: IO ()
 main = do
     result <- runTestTT allTests
@@ -11,7 +12,10 @@ main = do
 allTests :: Test 
 allTests = TestList [
     testTaxNothing,
-    testBaselineTax
+    testBaselineTax,
+    testHigherlineTax,
+    testUserTakings,
+    testAdditionallineTax
   ]
 
 testTaxNothing :: Test
@@ -19,3 +23,12 @@ testTaxNothing = TestCase (assertEqual "No tax is required" 0 (incomeTax 10000))
 
 testBaselineTax :: Test
 testBaselineTax = TestCase (assertEqual "The baseline tax rate" 1486 (incomeTax 20000))
+
+testHigherlineTax :: Test 
+testHigherlineTax = TestCase (assertEqual "The higherline tax rate" 15432 (incomeTax 70000))
+
+testAdditionallineTax :: Test 
+testAdditionallineTax = TestCase (assertEqual "The additionalline tax rate" 69932 (incomeTax 200000))
+
+testUserTakings :: Test
+testUserTakings =  TestCase (assertEqual "What the user is allowed" defaultUsersTakings (userTakings 40000))
